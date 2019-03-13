0e2e88a1cdd87a17db46589eecd39c32f20b6c46
Carlos J. Soto <csoto09@gmail.com>
2019-03-12 22:51:43 -0400

    merging node folders in my computer

--- terminal-commands/terminal-commands.js
+++ terminal-commands/terminal-commands.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const prompt = require('prompt')
 
 module.exports.ls = () => {
     fs.readdir('./', (err, files) => {
@@ -9,3 +10,23 @@ module.exports.ls = () => {
         console.log(filesToString);        
     })
 }
+
+module.exports.touch = () => {
+    prompt.start();
+    prompt.get(['fileName'], (err, result) => {
+        fs.writeFile(result.fileName,'','utf8',(err) => {
+            if (err) throw err;
+            console.log(`${result.fileName} created successfully.`);
+        })
+    })
+};
+  
+module.exports.mkdir = () => {
+    prompt.start();
+    prompt.get(['directory'], (err, result) => {
+        fs.mkdir(result.directory, (err) => {
+            if (err) throw err;
+            console.log(`${result.directory} created successfully.`);
+        })
+    })
+};
